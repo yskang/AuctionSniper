6da74ac6dc737811e1e5165052cae4456956baf7
Kang, Yongsung <yongsung.kang@gmail.com>
2013-01-05 11:50:50 +0900

    Correct the report price message in reportPrice
    Insert semicolon between Event and PRICE in the message of price report from server.
    This is the cause of above problem.

--- AuctionSniperTest/src/com/yskang/auctionsniper/test/FakeAuctionServer.java
+++ AuctionSniperTest/src/com/yskang/auctionsniper/test/FakeAuctionServer.java
@@ -55,7 +55,7 @@ public class FakeAuctionServer {
 
 	public void reportPrice(int price, int increment, String bidder)
 			throws XMPPException {
-		currentChat.sendMessage(String.format("SOLVersion: 1.1; Event PRICE; "
+		currentChat.sendMessage(String.format("SOLVersion: 1.1; Event: PRICE; "
 				+ "CurrentPrice: %d; Increment: %d; Bidder: %s;", price,
 				increment, bidder));
 	}
