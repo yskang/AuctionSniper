a0d7587594ed66c47ef758d8b1696cd160e79c5b
Yongsung Kang <yongsung.kang@gmail.com>
2013-01-15 16:16:58 +0900

    add test which is check the shiper list holds the snapshots in order

--- AuctionSniperTest/src/com/yskang/auctionsniper/unittest/SnipersTableAdapterTest.java
+++ AuctionSniperTest/src/com/yskang/auctionsniper/unittest/SnipersTableAdapterTest.java
@@ -5,6 +5,7 @@ import org.jmock.Mockery;
 
 import android.test.AndroidTestCase;
 
+import com.yskang.auctionsniper.Column;
 import com.yskang.auctionsniper.SniperSnapshot;
 import com.yskang.auctionsniper.SniperState;
 import com.yskang.auctionsniper.SnipersTableAdapter;
@@ -27,13 +28,12 @@ public class SnipersTableAdapterTest extends AndroidTestCase {
 
 		snipersTableAdapter = new SnipersTableAdapter(getContext());
 		snipersTableAdapter.registerDataSetObserver(auctionSnipersObserver);
-		
+
 		SniperSnapshot joining = SniperSnapshot.joining("item id");
 		SniperSnapshot bidding = SniperSnapshot.bidding(555, 666);
-		
+
 		snipersTableAdapter.addSniper(joining);
 		snipersTableAdapter.sniperStateChanged(bidding);
-		
 
 		assertEquals("item id", snipersTableAdapter.getItem(0).getItemId());
 		assertEquals(555, snipersTableAdapter.getItem(0).getLastPrice());
@@ -54,11 +54,29 @@ public class SnipersTableAdapterTest extends AndroidTestCase {
 
 		snipersTableAdapter = new SnipersTableAdapter(getContext());
 		snipersTableAdapter.registerDataSetObserver(auctionSnipersObserver);
-		
+
 		assertEquals(0, snipersTableAdapter.getRowCount());
 		snipersTableAdapter.addSniper(joining);
 		assertEquals(1, snipersTableAdapter.getRowCount());
 		assertEquals(snipersTableAdapter.getItem(0), joining);
+
+		context.assertIsSatisfied();
+	}
+
+	public void testHoldsSnipersInAdditionOrder() {
+		context.checking(new Expectations() {
+			{
+				ignoring(observer);
+			}
+		});
+		
+		snipersTableAdapter = new SnipersTableAdapter(getContext());
+		
+		snipersTableAdapter.addSniper(SniperSnapshot.joining("item 0"));
+		snipersTableAdapter.addSniper(SniperSnapshot.joining("item 1"));
+		
+		assertEquals("item 0", snipersTableAdapter.getItem(0).getItemId());
+		assertEquals("item 1", snipersTableAdapter.getItem(1).getItemId());
 		
 		context.assertIsSatisfied();
 	}
